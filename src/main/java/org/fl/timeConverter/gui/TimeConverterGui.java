809cd5b5892e79120dfb805a7dc0c2d3497717cb
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2019-08-09 13:28:44 +0200

    First version

--- src/main/java/org/fl/timeConverter/gui/TimeConverterGui.java
+++ src/main/java/org/fl/timeConverter/gui/TimeConverterGui.java
@@ -1,13 +1,32 @@
 package org.fl.timeConverter.gui;
 
-/**
- * Hello world!
- *
- */
-public class TimeConverterGui 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+
+public class TimeConverterGui  extends JFrame {
+	
+	private static final long serialVersionUID = 2368226038474247064L;
+	
+	private static final String DEFAULT_PROP_FILE = "file:///FredericPersonnel/PortableApps/TimeConverter/timeConverter.properties";
+
+	public TimeConverterGui(String defaultPropFile) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TimeConverterGui window = new TimeConverterGui(DEFAULT_PROP_FILE);
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
 }
