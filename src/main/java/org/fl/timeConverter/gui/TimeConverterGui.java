87df98eb146bb43e04b0cd39ca808dc28db52167
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2019-08-09 15:53:20 +0200

    Just add current time and log it

--- src/main/java/org/fl/timeConverter/gui/TimeConverterGui.java
+++ src/main/java/org/fl/timeConverter/gui/TimeConverterGui.java
@@ -1,8 +1,11 @@
 package org.fl.timeConverter.gui;
 
 import java.awt.EventQueue;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.Logger;
 
 import javax.swing.JFrame;
 
@@ -13,8 +16,10 @@ public class TimeConverterGui  extends JFrame {
 	
 	private static final long serialVersionUID = 2368226038474247064L;
 	
-	private static final String DEFAULT_PROP_FILE = "file:///FredericPersonnel/PortableApps/TimeConverter/timeConverter.properties";
+	private static final String DEFAULT_PROP_FILE = "timeConverter.properties";
 
+	private final static String datePattern = "uuuu-MM-dd HH:mm:ss.SSS VV" ;
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -30,13 +35,14 @@ public class TimeConverterGui  extends JFrame {
 	
 	public TimeConverterGui(String propertiesUri) {
 
-		try {
+		RunningContext runningContext = null ;
+		runningContext = new RunningContext("TimeConverter", null, propertiesUri);
+		
+		if (runningContext != null) {
 			
-			RunningContext runningContext = new RunningContext("TimeConverter", null, new URI(propertiesUri));
-			
-		} catch (URISyntaxException e) {
-			System.out.println("Exception caught in Main (see default prop file processing)") ;
-			e.printStackTrace();
+			Logger log = runningContext.getpLog() ;
+			String formattedDate = DateTimeFormatter.ofPattern(datePattern).format(ZonedDateTime.ofInstant(Instant.ofEpochMilli(System.currentTimeMillis()), ZoneId.systemDefault()));
+			log.info(formattedDate);
 		}
 	}
 
