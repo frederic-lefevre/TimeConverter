c7270e70f1b9191afce089fe9241c4cae8c6bac8
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2025-02-18 07:38:46 +0100

    Fix warning for unused import

--- src/main/java/org/fl/timeConverter/gui/MillisecondsAndZonePanel.java
+++ src/main/java/org/fl/timeConverter/gui/MillisecondsAndZonePanel.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
 
-Copyright (c) 2017, 2024 Frederic Lefevre
+Copyright (c) 2017, 2025 Frederic Lefevre
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -41,7 +41,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-import org.fl.timeConverter.Config;
 import org.fl.timeConverter.TimeUtils;
 
 public class MillisecondsAndZonePanel extends JPanel {
